24f978cce8dd9873c053dd09c5b6a85454d010f1
LEESEYUN <yatapboy@gmail.com>
2020-08-20 05:04:08 +0000

    run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-python tools/train_net.py --config-file configs/SKU110/retinanet_R_50_FPN_1x.yaml --num-gpus 2 SOLVER.IMS_PER_BATCH 16
+python tools/train_net.py --config-file configs/SKU110/retinanet_R_50_FPN_1x.yaml --num-gpus 2 SOLVER.IMS_PER_BATCH 4
