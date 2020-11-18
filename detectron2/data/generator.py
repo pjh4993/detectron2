9a081570449cc903efcb8167061b87417cf9e3e1
junho park <pjh3974@gmail.com>
2020-11-18 12:52:09 +0000

    generator modified

--- detectron2/data/generator.py
+++ detectron2/data/generator.py
@@ -20,7 +20,6 @@ class pascalVOCGenerator:
                 "depth": 3
             }),
             "segmented" : 0,
-            "object": []
         })
 
         pred_result = prediction["instances"].get_fields()
@@ -29,6 +28,24 @@ class pascalVOCGenerator:
         pred_classes = pred_result["pred_classes"]
         pred_level = pred_result["level"]
 
+        object_list = []
         for i in range(len(prediction)):
-            pass
+            box = pred_boxes[i].tensor.flatten()
+            object_list.append(dict({
+                "name": "object",
+                "pose": "Unspecified",
+                "truncated": 0,
+                "difficult": 0,
+                "bndbox": dict({
+                    "xmin":box[0].item(),
+                    "ymin":box[1].item(),
+                    "xmax":box[2].item(),
+                    "ymax":box[3].item(),
+                }),
+            }))
+        
+        result["annotation"]["object"] = object_list
+
+        return dicttoxml.dicttoxml(result)
+
        
